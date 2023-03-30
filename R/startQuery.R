13e086180caa1e8c46541d6f1abc5c0f5497d89a
edge-d <55300829+edge-d@users.noreply.github.com>
2023-03-30 16:02:13 -0700

    fixed app launch

--- R/startQuery.R
+++ R/startQuery.R
@@ -5,6 +5,6 @@
 #' @export
 startQueryApp <- function(port=8383) {
   shiny::runApp(host = getOption("shiny.host","0.0.0.0"),
-                port = port,
-                appDir = system.file('/R/app.R', package='QueryLipdverse'))
+                port = port)
 }
+#
